53b6dfa93281f6886f8cb8e85e801ef96029591e
joohwan <joohon812@nate.com>
2021-12-31 22:41:20 +0900

    텍스트 애니메이션 효과

--- src/dialog/Explain1.vue
+++ src/dialog/Explain1.vue
@@ -2,8 +2,9 @@
   <ExplainDialog>
     <template v-slot="{dialogClose}">
       <v-card>
+        <v-img src="https://raw.githubusercontent.com/joohwanJeon/quiz/master/src/assets/angry.gif" width="50%"
+        ></v-img>
         <v-card-title style="text-align: center" class="text-h5">
-          1번째 해설지
         </v-card-title>
         <v-card-actions>
           <v-spacer></v-spacer>
--- src/pages/Main.vue
+++ src/pages/Main.vue
@@ -2,23 +2,19 @@
   <div>
     <!-- <img width="100%" src="../assets/main.png"> -->
     <div style="position:absolute; top:45%; width:100%; text-align: center; color:white">
-      <p>
-        개발자가 된 것을 축하한다. 
-        이번 이벤트는 런닝맨 개발자 버전이다.
-      </p>
-      <p>
-        지금부터 너의 실력을 테스트하겠다. 
-        각 문제에서 제공하는 힌트를 조합해서 정답을 맞춰라.
-      </p>
-      <p>
-        힌트는 어디에나 있다.
-      </p>
-      <p>
-        그럼 행운을 빈다.
-      </p>
+      <h3>FrontEnd Developer 김희진 초대장</h3>
+      <div v-for="sentence in sentences" :key="sentence">
+        <span v-for="(word, index) in sentence" :key="index">{{word === ' '? '&nbsp;' : test(word)}}</span>
 
+      </div>
       <div>
-        <v-btn small @click="buttonClicked(text)" v-for="text in quizText" :key="text">{{text}}</v-btn>
+        <v-btn small @click="buttonClicked(text)" v-for="text in quizText1" :key="text">{{text}}</v-btn>
+      </div>
+      <div>
+        <v-btn small @click="buttonClicked(text)" v-for="text in quizText2" :key="text">{{text}}</v-btn>
+      </div>
+      <div>
+        <v-btn small @click="buttonClicked(text)" v-for="text in quizText3" :key="text">{{text}}</v-btn>
       </div>
       <v-btn @click="goQuiz" color="primary" :disabled="disabled">시작하기</v-btn>
 
@@ -37,12 +33,28 @@ export default {
   data() {
     return {
       disabled: true,
-      answer: process.env.VUE_APP_BASE_URL,
+      answer: process.env.VUE_APP_START_ANSWER,
       inputText: '',
-      quizText: ['주','부','전','태'],
+      quizText1: ['가','사','전','태'],
+      quizText2: ['환','퉤','해','헤'],
+      quizText3: ['희','랑','진','주'],
+      sentences: [''],
     }
   },
+  created() {
+    this.setDelay('지금부터 너의 개발 능력을 테스트 하겠다.', 2000);
+    this.setDelay('힌트는 어디에나 있다.', 4000);
+    this.setDelay('그럼 행운을 빈다.', 6000);
+  },
   methods: {
+    setDelay(sentence, time) {
+      setTimeout(() => {
+        this.sentences.push(sentence);
+      }, time)
+    },
+    test(word) {
+      return word === '\n' ? '<div></div>' : word
+    },
     buttonClicked(text) {
       this.inputText += text;
       if(this.answer.slice(0, this.inputText.length) !== this.inputText) {
@@ -61,3 +73,61 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+@import url(https://fonts.googleapis.com/css?family=Finger+Paint);
+
+body {
+  background: black;
+  overflow: hidden;
+  font: 5vw/100vh "Finger Paint";
+  text-align: center;
+  color: transparent;
+  backface-visibility: hidden;
+}
+
+span {
+  display: inline-block;
+  text-shadow: 0 0 0 whitesmoke;
+  animation: smoky 5s 3s both;
+}
+
+span:nth-child(even){
+  animation-name: smoky-mirror;
+}
+
+@keyframes smoky {
+  60% {
+    text-shadow: 0 0 40px whitesmoke;
+  }
+  to {
+    transform:
+      translate3d(15rem,-8rem,0)
+      rotate(-40deg)
+      skewX(70deg)
+      scale(1.5);
+    text-shadow: 0 0 20px whitesmoke;
+    opacity: 0;
+  }
+}
+
+@keyframes smoky-mirror {
+  60% {
+    text-shadow: 0 0 40px whitesmoke; }
+  to {
+    transform:
+      translate3d(18rem,-8rem,0)
+      rotate(-40deg) 
+      skewX(-70deg)
+      scale(2);
+     text-shadow: 0 0 20px whitesmoke;
+    opacity: 0;
+  }
+}
+
+@for $item from 1 through 23 {
+  span:nth-of-type(#{$item}){ 
+    animation-delay: #{(3 + ($item/10))}s; 
+  }
+} 
+</style>
